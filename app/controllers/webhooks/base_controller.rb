e4b8b8a46910b2b624753403d4944e5883895714
Colin Loretz <colin@orbit.love>
2023-04-22 12:46:00 -0700

    Added verification to Webhooks::BaseController

--- app/controllers/webhooks/base_controller.rb
+++ app/controllers/webhooks/base_controller.rb
@@ -1,6 +1,8 @@
 class Webhooks::BaseController < ApplicationController
   # Disable CSRF checking on webhooks because they do not originate from the browser
   skip_before_action :verify_authenticity_token
+  
+  before_action :verify_event
 
   def create
     InboundWebhook.create(body: payload)
@@ -8,6 +10,10 @@ class Webhooks::BaseController < ApplicationController
   end
 
   private
+
+  def verify_event
+    head :bad_request
+  end
 
   def payload
     @payload ||= request.body.read
