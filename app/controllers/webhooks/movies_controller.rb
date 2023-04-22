732e9ddc7fb5ef9f97e9c9b7e4dcde795ff29cc3
Colin Loretz <colin@orbit.love>
2023-04-22 13:02:59 -0700

    Added Movies sample verification to simulate failures

--- app/controllers/webhooks/movies_controller.rb
+++ app/controllers/webhooks/movies_controller.rb
@@ -1,12 +1,14 @@
 class Webhooks::MoviesController < Webhooks::BaseController
-    # A controller for catching new movie webhooks
+  # A controller for catching new movie webhooks
   #
   # To send a sample webhook locally:
   #
   #   curl -X POST http://localhost:3000/webhooks/movies
   #     -H 'Content-Type: application/json'
   #     -d '{"title":"Dungeons & Dragons: Honor Among Thieves"}'
-  #  
+  #
+  # Pass ?fail_verification=1 to simulate a webhook verification failure
+
   def create
     # Save webhook to database
     record = InboundWebhook.create!(body: payload)
@@ -18,6 +20,11 @@ class Webhooks::MoviesController < Webhooks::BaseController
   end
 
   private
+
+  # Pass ?fail_verification=1 to simulate a webhook verification failure
+  def verify_event
+    head :bad_request if params[:fail_verification]
+  end
 
   def payload
     @payload ||= request.body.read
