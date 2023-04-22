2be18da4fe36cdf0f819b8498e83ffad8dd731cb
Colin Loretz <colin@orbit.love>
2023-04-22 12:48:57 -0700

    Added verification to Webhooks::StripeController

--- app/controllers/webhooks/stripe_controller.rb
+++ app/controllers/webhooks/stripe_controller.rb
@@ -12,6 +12,21 @@ class Webhooks::StripeController < Webhooks::BaseController
 
   private
 
+  # Verifies the event came from Stripe
+  def verify_event
+    signature = request.headers['Stripe-Signature']
+    secret = Rails.application.credentials.dig(:stripe, :webhook_signing_secret)
+
+    ::Stripe::Webhook::Signature.verify_header(
+      payload,
+      signature,
+      secret.to_s,
+      tolerance: Stripe::Webhook::DEFAULT_TOLERANCE,
+    )
+  rescue ::Stripe::SignatureVerificationError
+    head :bad_request
+  end
+
   def payload
     @payload ||= request.body.read
   end
