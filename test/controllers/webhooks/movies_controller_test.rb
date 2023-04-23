95c76bedac06f8856d8ef26f06a3da3c0be748cc
Colin Loretz <colin@orbit.love>
2023-04-23 10:18:52 -0400

    Added simple test for movies webhook

--- test/controllers/webhooks/movies_controller_test.rb
+++ test/controllers/webhooks/movies_controller_test.rb
@@ -1,7 +1,20 @@
-require "test_helper"
+require 'test_helper'
 
 class Webhooks::MoviesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    # Load the webhook data from the JSON file
+    file_path = Rails.root.join('test', 'fixtures', 'webhooks', 'movie.json')
+    @webhook = JSON.parse(File.read(file_path))
+  end
+
+  test 'should consume webhook' do
+    # Send the POST request to the create action with the prepared data
+    post webhooks_movies_url, params: @webhook
+
+    # Check if the response status is 200 OK
+    assert_response :ok
+
+    # You can create other test files for each job or service that is called
+    # For example, check if a record was created/updated in the database
+  end
 end
