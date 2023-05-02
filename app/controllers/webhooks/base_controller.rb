bfef81359c41693d8654df5a0e0acfd0c34ae673
Chris Oliver <excid3@gmail.com>
2023-05-02 09:17:58 -0500

    Remove CSRF skip because API controllers don't include that by default

--- app/controllers/webhooks/base_controller.rb
+++ app/controllers/webhooks/base_controller.rb
@@ -1,7 +1,4 @@
 class Webhooks::BaseController < ActionController::API
-  # Disable CSRF checking on webhooks because they do not originate from the browser
-  skip_before_action :verify_authenticity_token
-  
   before_action :verify_event
 
   def create
